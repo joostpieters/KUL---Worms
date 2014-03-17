d65130cdb68ba870e9ce1baf250fe98f37092e21
Divyak156 <kristof.achten@student.kuleuven.be>
2014-03-17 23:51:41 +0100

    Test return succesful now.

--- 1314-Worms-Part1/src-provided/worms/model/Facade.java
+++ 1314-Worms-Part1/src-provided/worms/model/Facade.java
@@ -10,9 +10,6 @@ package worms.model;
 
 public class Facade implements IFacade {
 
-	public Facade(){
-		//This is the default constructor
-	}
 	
 	@Override
 	public Worm createWorm(double x, double y, double direction, double radius, String name) throws IllegalArgumentException{
@@ -27,92 +24,92 @@ public class Facade implements IFacade {
 
 	@Override
 	public boolean canMove(Worm worm, int nbSteps) {
-		return Worm.canMove(worm, nbSteps);
+		return worm.canMove(nbSteps);
 	}
 
 	@Override
 	public void move(Worm worm, int nbSteps) {
-		Worm.move(worm, nbSteps);
+		worm.move(nbSteps);
 	}
 
 	@Override
 	public boolean canTurn(Worm worm, double angle) {
-		return Worm.canTurn(worm, angle);
+		return worm.canTurn(angle);
 	}
 
 	@Override
 	public void turn(Worm worm, double angle) {
-		Worm.turn(worm, angle);
+		worm.turn(angle);
 	}
 
 	@Override
-	public void jump(Worm worm) {
-		Worm.jump(worm);
+	public void jump(Worm worm){
+			worm.jump();
 	}
 
 	@Override
 	public double getJumpTime(Worm worm) {
-		return Worm.getJumpTime(worm);
+		return worm.getJumpTime();
 	}
 
 	@Override
 	public double[] getJumpStep(Worm worm, double t) {
-		return Worm.getJumpStep(worm, t);
+		return worm.getJumpStep(t);
 	}
 
 	@Override
 	public double getX(Worm worm) {
-		return Worm.getX(worm);
+		return worm.getX();
 	}
 
 	@Override
 	public double getY(Worm worm) {
-		return Worm.getY(worm);
+		return worm.getY();
 	}
 
 	@Override
 	public double getOrientation(Worm worm) {
-		return Worm.getOrientation(worm);
+		return worm.getOrientation();
 	}
 
 	@Override
 	public double getRadius(Worm worm) {
-		return Worm.getRadius(worm);
+		return worm.getRadius();
 	}
 
 	@Override
 	public void setRadius(Worm worm, double newRadius) {
-		Worm.setRadius(worm, newRadius);
+		worm.setRadius(newRadius);
 	}
 
 	@Override
 	public double getMinimalRadius(Worm worm) {
-		return Worm.getMinimalRadius(worm);
+		return worm.getMinimalRadius();
 	}
 
 	@Override
 	public int getActionPoints(Worm worm) {
-		return Worm.getActionPoints(worm);
+		return worm.getActionPoints();
 	}
 
 	@Override
 	public int getMaxActionPoints(Worm worm) {
-		return Worm.getMaxActionPoints(worm);
+		return worm.getMaxActionPoints();
 	}
 
 	@Override
 	public String getName(Worm worm) {
-		return Worm.getName(worm);
+		return worm.getName();
 	}
 
 	@Override
 	public void rename(Worm worm, String newName) {
-		Worm.rename(worm, newName);
+		worm.rename(newName);
 	}
 
 	@Override
 	public double getMass(Worm worm) {
-		return Worm.getMass(worm);
+		return worm.getMass();
 	}
 
 }
--- 1314-Worms-Part1/src-provided/worms/model/Worm.java
+++ 1314-Worms-Part1/src-provided/worms/model/Worm.java
@@ -20,11 +20,11 @@ import be.kuleuven.cs.som.annotate.Immutable;
 
 public class Worm {
 	
-	private static double x, y, direction, radius;
-	private static String name;
-	private static int actionPoints;
+	private double x, y, direction, radius, mass;
+	private String name;
+	private int actionPoints;
 
-	/**
+	/*
 	 * Initialize a Worm with given position, direction, radius and name.
 	 * 
 	 * @param  	x
@@ -49,45 +49,29 @@ public class Worm {
 	
 	public Worm(double x, double y, double direction, double radius, String name){
 	
-		Worm.x = x;
-		Worm.y = y;
-		Worm.direction = direction;
+		setX(x);
+		setY(y);
+		setOrientation(direction);
 		if(Worm.isValidName(name)){
-			Worm.name = name;
+			rename(name);
 		}
 		else{
 			System.out.println("This name is not correct. The new name may only contain alphabetical letters, spaces and quotationmarks. Your worm will be given a preset name.");
-			int i = (int)((Math.random()*7)+1);
-			switch(i){		
-			case 1:
-				Worm.name = "Conor O'Brian";
-				break;
-			case 2:
-				Worm.name = "Cap'n Transalore";
-				break;
-			case 3:
-				Worm.name = "Tom Baker";
-				break;
-			case 4:
-				Worm.name = "David Smith";
-				break;
-			case 5:
-				Worm.name = "Matthew O'Hare";
-				break;
-			case 6:
-				Worm.name = "Procra Stinator";
-				break;
-			case 7:
-				Worm.name = "Jack Harkness";
-				break;
-			default:
-				Worm.name = "Geoff Mermaid";
 			}
+		if(radius<=0.25){
+			setRadius(0.25);
 		}
-		if(radius>=0.25){
-			Worm.radius = 0.25;
-		}
-		else{Worm.radius = radius;}
+		else{setRadius(radius);}
+		setActionPoints(getMaxActionPoints());
+
+	}
+	
+	public void setX(double x){
+		this.x = x;
+	}
+	
+	public void setY(double y){
+		this.y = y;
 	}
 	
 	public static boolean isValidName(String name){
@@ -97,50 +81,43 @@ public class Worm {
 	/**
 	 * Return the mass of a worm in kilograms.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the mass needs to be returned.
 	 * @return	The current mass of given worm.
 	 * 		  | result == mass	
 	 */
 	
 	@Basic
-	public static double getMass(Worm worm){	
-		return 1062*((4/3)*Math.PI*Math.pow(Worm.radius, 3));
+	public double getMass(){
+		this.mass = (1062*((4*Math.PI*Math.pow(getRadius(), 3))/3));
+		return mass ;
 	}
 	
 	/**
 	 * Return the radius of a worm in metres.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the radius needs to be returned.
 	 * @return	The current radius of given worm.
 	 * 	      | result == initialRadius
 	 */
 	
 	@Basic
-	public static double getRadius(Worm worm){
-		return Worm.radius;
+	public double getRadius(){
+		return radius;
 	}
 	
 	/**
 	 * Return the minimal radius a Worm needs to have.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the minimal radius needs to be returned.
 	 * @return	The minimal radius of given worm.
 	 * 		  | result == 0.25
 	 */
 	
 	@Basic @Immutable
-	public static double getMinimalRadius(Worm worm){
+	public double getMinimalRadius(){
 		return 0.25;
 	}
 	
 	/**
 	 * Change or set the radius of a Worm to the provided radius.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the radius needs to be set or changed.
 	 * @param 	newRadius
 	 * 			The new radius the worm needs to be in metres.
 	 * @pre		The new radius of the worm must be at least 0.25 metres.
@@ -150,106 +127,94 @@ public class Worm {
 	 * @effect	The radius of the worm will be changed.
 	 */
 	
-	public static void setRadius(Worm worm, double newRadius){
-		if(newRadius < 0.25){
-			Worm.setRadius(worm, 0.25);
+	public void setRadius(double newRadius){
+		if(newRadius <= 0.25){
+			this.radius = 0.25;
 		}
 		else{
-			Worm.setRadius(worm, newRadius);
+			this.radius = newRadius;
 			}
+	}
+	
+	public void setOrientation(double direction){
+		this.direction = direction;
 	}
 	
 	/**
 	 * Return the current orientation of a worm.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the orientation needs to be returned.
 	 * @return	The current direction the worm is oriented at.
 	 * 		  | result == initialDirection
 	 */
 	
 	@Basic
-	public static double getOrientation(Worm worm){
-		return Worm.direction;
+	public double getOrientation(){
+		return direction;
 	}
 	
 	/**
 	 * Return the X-coordinate of a worm.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the X-coordinate needs to be returned.
 	 * @return 	The X-position of given worm.
 	 * 		  | result == xPos
 	 */
 	
 	@Basic
-	public static double getX(Worm worm){
-		return Worm.x;
+	public double getX(){
+		return x;
 	}
 	
 	/**
 	 * Return the Y-coordinate of a worm.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the Y-coordinate needs to be returned.
 	 * @return	The Y-position of given worm.
 	 * 		  | result == yPos
 	 */
 	
 	@Basic
-	public static double getY(Worm worm){
-		return Worm.y;
+	public double getY(){
+		return y;
 	}
 	
 	/**
 	 * Return the name of a worm.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the name needs to be returned.
 	 * @return	The name of given worm.
 	 * 		  | result.equals(initialName)
 	 */
 	
 	@Basic
-	public static String getName(Worm worm){
-		return Worm.name;
+	public String getName(){
+		return name;
 	}
 	
 	/**
 	 * Return the maximum of actionpoints a worm can have.
 	 * 
-	 * @param 	worm
-	 * 			The worm of whom the maximum actionpoints need to be returned.
 	 * @return 	The maximum actionpoints given worm can have.
 	 * 		  |	result == initialActionPoints
 	 */
 	
-	@SuppressWarnings("static-access")
 	@Basic
-	public static int getMaxActionPoints(Worm worm){
-		Worm.actionPoints = (int)(worm.getMass(worm));
-		return (int)(worm.getMass(worm));
+	public int getMaxActionPoints(){
+		return (int)(getMass());
 	}
 	
 	/**
 	 * Return the current actionspoint of a worm that are left.
 	 * 
-	 * @param 	worm
-	 *			The worm of whom the actionpoints need to be returned.
 	 * @return	The current number of actionpoints given worm has.
 	 * 		  |	result == actionPoints
 	 */
 	
 	@Basic
-	public static int getActionPoints(Worm worm){
-		return Worm.actionPoints;
+	public int getActionPoints(){
+		return actionPoints;
 	}
 	
 	/**
 	 * Give a worm a new name that has to abide certain standards.
 	 *  
-	 * @param 	worm
-	 * 			The worm of whom we want the name to be changed..
 	 * @param 	newName
 	 * 			The new name we would like to give to given worm.
 	 * @pre		The name must be a valid name.
@@ -259,9 +224,9 @@ public class Worm {
 	 * @effect	The worm will be given a nem name.
 	 */
 	
-	public static void rename(Worm worm, String newName){
-			if(Worm.isValidName(newName)){
-				Worm.name = newName;
+	public void rename(String newName){
+			if(isValidName(newName)){
+				name = newName;
 			}
 			else{
 				System.out.println("This name is not correct. The new name may only contain alphabetical letters, spaces and quotationmarks.");
@@ -272,8 +237,6 @@ public class Worm {
 	/**
 	 * Check whether a worm is able to move.
 	 * 
-	 * @param 	worm
-	 * 			The worm of who we are checking whether he is able to move a certain amount of steps.
 	 * @param 	steps
 	 * 			The amount of steps the worm want to make
 	 * @return	True if the amount of steps times the cost each step takes is smaller than the available actionpoints.
@@ -281,11 +244,14 @@ public class Worm {
 	 */
 	
 	@Basic
-	public static boolean canMove(Worm worm, int steps){
-		boolean canMove = true;
-		int stepCost = (int) ((int)(Math.ceil(Math.abs(Math.cos(Worm.getOrientation(worm)))))+(Math.abs(4*Math.sin(Worm.getOrientation(worm)))));
-		if((steps*stepCost) > Worm.getActionPoints(worm)){
+	public boolean canMove(int steps){
+		boolean canMove;
+		int stepCost = (int)(Math.abs(Math.cos(getOrientation()))+(Math.abs(Math.sin(getOrientation())*4)));
+		if((steps*stepCost) > getActionPoints()){
 			canMove = false;
+		}
+		else{
+			canMove = true;
 		}
 		return canMove;
 	}
@@ -293,20 +259,18 @@ public class Worm {
 	/**
 	 * Make a worm move a certain amount of steps conform to his current orientation.
 	 * 
-	 * @param	worm
-	 * 			The worm who we are going to move.		
 	 * @param 	steps
 	 * 			The amount of steps given worm is going to move.
 	 * @effect	The worm will move given amount of steps.
 	 */
 	
-	public static void move(Worm worm, int steps){
+	public void move(int steps){
 		try{
-		if(Worm.canMove(worm, steps) == true){
-			int stepCost = (int)(Math.ceil(Math.abs(Math.cos(Worm.getOrientation(worm))))+(Math.abs(4*Math.sin(Worm.getOrientation(worm)))));
-			Worm.actionPoints -= (steps*stepCost);
-			Worm.x += (steps*(Math.cos(Worm.getOrientation(worm))*Worm.getRadius(worm)));
-			Worm.x += (steps*(Math.sin(Worm.getOrientation(worm))*Worm.getRadius(worm)));
+		if(canMove(steps)){
+			int stepCost = (int)(Math.abs(Math.cos(getOrientation()))+(Math.abs(Math.sin(getOrientation())*4)));
+			setActionPoints(getActionPoints() - (steps*stepCost));
+			setX(getX() + (steps*Math.cos(getOrientation())*getRadius()));
+			setY(getY() + (steps*Math.sin(getOrientation())*getRadius()));
 		}
 		else{
 			System.out.print("You do not have enough actionpoint to make this move.");
@@ -317,11 +281,13 @@ public class Worm {
 		}
 	}
 	
+	public void setActionPoints(int actionPoints){
+		this.actionPoints = actionPoints;
+	}
+	
 	/**
 	 * Check whether a worm is able to turn.
 	 * 
-	 * @param 	worm
-	 * 			The worm we anticipate on turning.
 	 * @param 	angle
 	 * 			The angle we would like to turn given worm with.
 	 * @return	True if the cost of the turn is smaller than the actionpoints that remain.
@@ -329,10 +295,10 @@ public class Worm {
 	 */
 	
 	@Basic
-	public static boolean canTurn(Worm worm, double angle){
+	public boolean canTurn(double angle){
 		boolean canTurn = true;
 		int turnCost = (int)(Math.ceil(angle*(60/(2*Math.PI))));
-		if(turnCost > Worm.getActionPoints(worm)){
+		if(turnCost > getActionPoints()){
 			canTurn = false;
 		}
 		return canTurn;
@@ -341,54 +307,46 @@ public class Worm {
 	/**
 	 * Make a worm turn by adding or subtracting a certain angle from/to its current direction.
 	 * 
-	 * @param 	worm
-	 * 			The worm who we are going to turn a certain angle.
 	 * @param 	angle
 	 * 			The angle we would like given worm to turn.
 	 * @effect	The worm will turn given angle.
 	 */
 	
-	public static void turn(Worm worm, double angle){
-		if(Worm.canTurn(worm, angle)){ //TODO: Add turncost
-		Worm.direction += (angle);
+	public void turn(double angle){
+		if(canTurn(angle)){ //TODO: Add turncost
+		direction += (angle);
 		}
 	}
 	
 	/**
 	 * Return the Force that's going to be exerted on a worm during a jump.
 	 * 
-	 * @param 	worm
-	 * 			The worm to calculate the force for.
 	 * @return	The force that needs to be exerted given said conditions.
 	 * 		  |	result == ((5*actionPoints)+(mass*9.80665))
 	 */
 	
 	@Basic
-	public static double getJumpForce(Worm worm){
-		return ((5*actionPoints)+(Worm.getMass(worm)*9.80665));
+	public double getJumpForce(){
+		return ((5*actionPoints)+(getMass()*9.80665));
 	}
 
 	/**
 	 * Return the time it will take to potentially make a jump with respect to the worms details.
 	 * 
-	 * @param 	worm
-	 * 			The worm to calculate the potential jump time for.
 	 * @return	The time a potential jump will take.
 	 * 		  |	result == distance/(initialVelocity*cos(initialDirection))
 	 */
 	
 	@Basic
-	public static double getJumpTime(Worm worm){
-		double initialVelocity = ((Worm.getJumpForce(worm)*0.5)/Worm.getMass(worm));
-		double distance = (((initialVelocity*initialVelocity)*Math.sin(2*Worm.getOrientation(worm)))/9.80665);
-		return distance/(initialVelocity*Math.cos(Worm.getOrientation(worm)));
+	public double getJumpTime(){
+		double initialVelocity = ((getJumpForce()*0.5)/getMass());
+		double distance = (((initialVelocity*initialVelocity)*Math.sin(2*getOrientation()))/9.80665);
+		return distance/(initialVelocity*Math.cos(getOrientation()));
 	}
 	
 	/**
 	 * Return the X and Y coordinates of a worm performing a jump at a given time.
 	 * 
-	 * @param 	worm
-	 * 			The worm to calculate the potential jump trajectory for.
 	 * @param 	time
 	 * 			The time at we would like to check the position of the worm in flight.
 	 * @return	The X and Y coordinates of given worm at given time, neatly ordened in an array.
@@ -396,37 +354,36 @@ public class Worm {
 	 */
 	
 	@Basic
-	public static double[] getJumpStep(Worm worm, double time){
+	public double[] getJumpStep(double time){
 		double[] positionPerTime = new double[2];
-		double initialXVelocity = (((Worm.getJumpForce(worm)*0.5)/Worm.getMass(worm))*Math.cos(Worm.getOrientation(worm)));
-		double initialYVelocity = (((Worm.getJumpForce(worm)*0.5)/Worm.getMass(worm))*Math.sin(Worm.getOrientation(worm)));
-		positionPerTime[0] = (Worm.getX(worm)+(initialXVelocity*time));
-		positionPerTime[1] = (Worm.getY(worm)+((initialYVelocity*time)-((9.80665*time*time/2))));
+		double initialXVelocity = (((getJumpForce()*0.5)/getMass())*Math.cos(getOrientation()));
+		double initialYVelocity = (((getJumpForce()*0.5)/getMass())*Math.sin(getOrientation()));
+		positionPerTime[0] = (getX()+(initialXVelocity*time));
+		positionPerTime[1] = (getY()+((initialYVelocity*time)-((9.80665*time*time/2))));
 		return positionPerTime;
 	}
 	
 	/**
 	 * Make a worm jump in a physical trajectory according to gravitation.
 	 * 
-	 * @param 	worm
-	 * 			The worm that is going to jump.
 	 * @effect	The worm will have jumped unless he was facing downwards.	
 	 */
 	
-	public static void jump(Worm worm){
-		double initialVelocity = ((Worm.getJumpForce(worm)*0.5)/getMass(worm));
-		double distance = (((initialVelocity*initialVelocity)*Math.sin(2*Worm.getOrientation(worm)))/9.80665);
-		if(Worm.getOrientation(worm)>=0 && Worm.getOrientation(worm)<(Math.PI/2)){
-			Worm.x += distance;
+	public void jump() throws IllegalArgumentException{
+		double initialVelocity = ((getJumpForce()*0.5)/getMass());
+		double distance = (((initialVelocity*initialVelocity)*Math.sin(2*getOrientation()))/9.80665);
+		if(getOrientation()>=0 && getOrientation()<(Math.PI/2)){
+			setX(getX()-distance);
+			setActionPoints(0);
 		}
-		else if(Worm.getOrientation(worm)>(Math.PI/2) && Worm.getOrientation(worm) <= Math.PI){
-			Worm.y -= distance;
+		else if(getOrientation()>(Math.PI/2) && getOrientation() <= Math.PI){
+			setY(getY() - distance); 
+			setActionPoints(0);
 		}
 		else{
-			System.out.println("Jumping at this angle was not such a good idea.");
-		}
+				throw new ModelException("Unable to jump");
+			}
 		
-		actionPoints = 0;
 		
 	}
 }
